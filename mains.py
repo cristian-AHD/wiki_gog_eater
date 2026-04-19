878ab8146d90e458e17fa1e0f4130a1c1108cdcf
Cristian Ahumada <cdahumada35@ucatolica.edu.co>
2026-04-19 17:50:09 -0500

    crud

--- mains.py
+++ mains.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI, HTTPException
-from models import (
+from model import (
     Aragami, AragamiCreate,
     GodArc, GodArcCreate,
     GodEater, GodEaterCreate,
 )
+
+app = FastAPI()
+
+
+@app.get("/aragami", response_model=list[Aragami], tags=["Aragami"])
+def listar_aragami():
+    return aragami_db.get_all()
+
+
+@app.get("/aragami/{aragami_id}", response_model=Aragami, tags=["Aragami"])
+def obtener_aragami(aragami_id: int):
+    obj = aragami_db.get_by_id(aragami_id)
+    if not obj:
+        raise HTTPException(status_code=404, detail="Aragami no encontrado")
+    return obj
