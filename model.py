95b0920c818dcb119282e024b40bcb2686242450
Cristian Ahumada <cdahumada35@ucatolica.edu.co>
2026-04-19 16:50:30 -0500

    Elementos

--- model.py
+++ model.py
@@ -1,12 +1,12 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from enum import Enum
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  #
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class ElementoEnum(str, Enum):
+    fuego = "Fuego"
+    agua = "Agua"
+    rayo = "Rayo"
+    hielo = "Hielo"
+    divino = "Divino"
+    neutro = "Neutro"
